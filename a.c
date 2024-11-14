d16798af2c5e1271637fc1ab12754220e4f8f19c
Lava195 <bt23ece129@iiitn.ac.in>
2024-11-14 21:41:15 +0530

    Update a.c

--- a.c
+++ a.c
@@ -1,3 +1,14 @@
 a.c
   this is my first programe
 hiii
+#include<stdio.h>
+int main(){
+  int a,b,c;
+printf("enter the first number");
+scanf("%d",&a);
+printf("enter the second number");
+scanf("%d",&b);
+c=a+b;
+printf("%d",c");
+return 0;
+}
